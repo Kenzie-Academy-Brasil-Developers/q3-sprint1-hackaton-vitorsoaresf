55d2e3becf4c5c431c15001a7e465f607fd6a760
vitorsoaresf <vitorsoaresf@gmail.com>
2022-02-09 10:13:20 -0300

    feat: implements func get_score

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,7 @@
-# Seu código aqui
+
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+def get_score(team_name, teams):
+    return f"A Team Ateliware ficou classificada em {teams.index(team_name) + 1}" 

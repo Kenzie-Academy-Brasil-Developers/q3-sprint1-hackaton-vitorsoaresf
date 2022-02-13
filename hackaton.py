60a3c0429df71a1a94b39dc0483faf49312bf82b
vitorsoaresf <vitorsoaresf@gmail.com>
2022-02-13 07:21:58 -0300

    fix: refactore code

--- hackaton.py
+++ hackaton.py
@@ -4,4 +4,4 @@ hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
 hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
 
 def get_score(team_name, teams):
-    return f"A Team Ateliware ficou classificada em {teams.index(team_name) + 1}" 
+    return f"A {team_name} ficou classificada em {teams.index(team_name) + 1}" 
